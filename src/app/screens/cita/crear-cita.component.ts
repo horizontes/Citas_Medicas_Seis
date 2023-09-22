6872da9b39ae3ac4795a6f3b2f8793a2265980e0
Christopher Garrido <cgn_nice@msn.com>
2023-09-22 14:44:51 -0500

    crear cita stepper

--- src/app/screens/cita/crear-cita.component.ts
+++ src/app/screens/cita/crear-cita.component.ts
@@ -21,22 +21,25 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
+import {MatChipsModule} from '@angular/material/chips';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter'
 
 //import data
 import Especialidades from '../../../assets/data/especialidades.json';
 import Medicos from '../../../assets/data/medicos.json';
 import Sedes from '../../../assets/data/sedes.json';
 import Citas from '../../../assets/data/citas.json';
+import * as _moment from 'moment';
 
 @Component({
   selector: 'crearCita',
   templateUrl: './crear-cita.component.html',
   styleUrls: ['./crear-cita.component.scss'],
   providers: [
-    {
-      provide: STEPPER_GLOBAL_OPTIONS,
-      useValue: {showError: true},
-    },
+    { provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}, },
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ],
   standalone: true,
   imports: [
@@ -60,6 +63,7 @@ import Citas from '../../../assets/data/citas.json';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatChipsModule,
   ],
 })
 
@@ -68,21 +72,33 @@ export class CrearCitaComponent implements OnInit{
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
+
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
 
+  datePipe: DatePipe = new DatePipe("en-US");
+
+  disabledFirstNext : boolean = true;
+  disabledSecondNext : boolean = true;
+  disabledThirdNext : boolean = true;
+
+  date = new FormControl(_moment());
+
   fechaSelected: Date = new Date;
   fechaStringSelected: string | null = "";
 
-  especialidadSelected : Especialidad = { name:"", id:0 };
-  medicoSelected : Medico = { name:"", medicoId:0, especialidadId:0, sedeId: [], colaPacientes: 0 };
-  sedeSelected: Sede = { name: "", id: 0,  district: "", address: "", reference: "" };
-
   especialidades: Especialidad[] = Especialidades;
+  especialidadSelected : Especialidad = { name:"", id:0 };
+
   medicos: Medico[] = Medicos;
   optionsMedicos: Array<Medico[]> = [];
+  medicoSelected : Medico = { name:"", medicoId:0, especialidadId:0, sedeId: [], colaPacientes: 0, 
+    citas:[], horasLibres:[] };
+
   sedes: Sede[] = Sedes;
+  sedeSelected: Sede = { name: "", id: 0,  district: "", address: "", reference: "" };
+  
   citas: Citas[] = Citas;
 
   constructor(private _formBuilder: FormBuilder, private matIconRegistry: MatIconRegistry,
@@ -103,12 +119,6 @@ export class CrearCitaComponent implements OnInit{
 
           this.medicos = this.medicos.filter(m => m.especialidadId == this.especialidadSelected.id);
           this.optionsMedicos = this.converTwoDimensions(this.optionsMedicos, this.medicos);
-
-          this.citas.forEach(c => {
-            if (this.medicos.filter(m => m.medicoId == c.medicoId)[0] != null) {
-              this.medicos.filter(m => m.medicoId == c.medicoId)[0].colaPacientes = c.duracion;
-            }
-          });
           
         }
         
@@ -138,11 +148,7 @@ export class CrearCitaComponent implements OnInit{
 
   }
 
-  myControl = new FormControl('');
-
-  filteredOptions: Observable<string[]> = new Observable;
-  optionsFiltered: Array<any> = [];
-  
+  myControl = new FormControl('');  
 
   ngOnInit() {
   }
@@ -168,12 +174,55 @@ export class CrearCitaComponent implements OnInit{
 
   obtenerFecha(e: any){
 
-    let datePipe: DatePipe = new DatePipe("en-US");
+    if (e.value != null) {
 
-    if (datePipe.transform(e.value as Date, "dd/MM/yyyy") != null) {
-      this.fechaStringSelected = datePipe.transform(e.value as Date, "dd/MM/yyyy");
+      this.fechaStringSelected = this.transformDate(e.value);
+      this.obtenerCitas(this.fechaStringSelected);
+
     }
     
+  }
+
+  obtenerCitas(f: string | null){
+
+    if (this.medicos != null){
+      for (let i = 0; i < this.medicos.length; i++) {
+        if (this.medicos[i] != null){
+          const m = this.medicos[i];
+          m.colaPacientes = 0;
+          for (let i = 0; i < this.citas.length; i++) {
+            const c = this.citas[i];
+            if (c.medicoId == m.medicoId && this.fechaStringSelected == c.fecha){
+              m.citas.push(c);
+              m.colaPacientes = +c.duracion;
+            }
+          }
+          this.medicos[i] = m;
+        }        
+      }
+    }
+  }
+
+  obtenerMedico(e: any){
+
+    if (e.value != null) {
+      this.medicoSelected = e.value;
+      this.disabledFirstNext = false;
+    }
+
+  }
+
+  obtenerHorasLibres(m : Medico){
+
+    if (m != null){
+      const f = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+      for (let index = 0; index < f.length; index++) {
+        y.hora.split(':')[0];
+          m.horasLibres = f.filter(x => m.citas.filter(y => x == y.hora.split(':')[0] as number));
+      }
+
+    }
+
   }
 
   private converTwoDimensions(optionsFiltered: Array <any[]>, options: any[]){
@@ -194,6 +243,12 @@ export class CrearCitaComponent implements OnInit{
 
   }
 
+  private transformDate(d : Date){
+
+    return this.datePipe.transform(d as Date, "MM/dd/yyyy");
+
+  }
+
 }
 
 interface Medico {
@@ -203,6 +258,8 @@ interface Medico {
   especialidadId: number;
   sedeId: number[];
   colaPacientes: number;
+  horasLibres: number[];
+  citas: Citas[];
 
 }
 
@@ -228,6 +285,7 @@ interface Citas{
   fecha: string;
   hora: string;
   duracion: number;
+  total: number;
 
 }
 
